791b85fedaaf5cf7efab56b770c29358bbf3bc5e
Malinovskii Vladimir <galqiwi@gmail.com>
2025-02-13 20:27:58 +0300

    upd

--- src/transformers/quantizers/quantizer_higgs.py
+++ src/transformers/quantizers/quantizer_higgs.py
@@ -122,10 +122,13 @@ class HiggsHfQuantizer(HfQuantizer):
         module_name = ".".join(param_name.split(".")[:-1])
         for key, value in flute_dict.items():
             if key in module._parameters:
+                print(1, key, value.device, value.shape)
                 module._parameters[key] = torch.nn.Parameter(value, requires_grad=False)
             elif key in module._buffers:
+                print(2, key, value.device, value.shape)
                 module._buffers[key] = torch.nn.Buffer(value)
             elif key == "tune_metadata":
+                print(3, key, value)
                 module.tune_metadata = value
                 self.quantization_config.tune_metadata[module_name] = value.to_dict()
             else:
